2c12a2aa7c31eb53a1b60ce10098dcfe99d2bd9b
Pranav Bakale <78615142+pranavbakale@users.noreply.github.com>
2021-09-06 21:16:36 +0530

    Create 2.2-fibonacci_last_digit.cpp

---
+++ Week 2/2.2-fibonacci_last_digit.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+long long fibonacci(int n)
+{
+	vector<long long> fib(n + 1);
+	fib[0] = 0;
+	fib[1] = 1;
+	for (int i = 2; i <= n; i++)
+    {
+        fib[i] = (fib[i - 1]%10 + fib[i - 2]%10)%10;
+    }
+    return fib[n];
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	cout << fibonacci(n) << endl;
+}
