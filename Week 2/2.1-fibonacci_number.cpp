b88563003aecf000347b139f8df6815795efa370
Pranav Bakale <78615142+pranavbakale@users.noreply.github.com>
2021-09-06 21:15:59 +0530

    Create 2.1-fibonacci_number.cpp

---
+++ Week 2/2.1-fibonacci_number.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+long long fibCalc(int n)
+{
+	vector<long long> fib(n + 1);
+	fib[0] = 0;
+	fib[1] = 1;
+	for (int i = 2; i <= n; i++)
+	{
+		fib[i] = fib[i - 1] + fib[i - 2];
+	}
+	return fib[n];
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	cout << fibCalc(n) << endl;
+}
