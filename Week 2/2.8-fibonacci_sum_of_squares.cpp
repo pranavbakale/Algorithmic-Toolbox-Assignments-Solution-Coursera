0513e716247b4cca26a7b06fbd0e5848fffe1348
Pranav Bakale <78615142+pranavbakale@users.noreply.github.com>
2021-09-06 21:20:28 +0530

    Create 2.8-fibonacci_sum_of_squares.cpp

---
+++ Week 2/2.8-fibonacci_sum_of_squares.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+long long periodic(long long m) {
+	long long a = 0, b = 1, c = a + b;
+	for (int i = 0; i < m*m; i++) {
+		c = (a + b) % m;
+		a = b;
+		b = c;
+		if (a == 0 && b == 1)
+			return i + 1;
+	}
+}
+
+int solve(long long n, long long m) {
+	long long remainder = n % periodic(m);
+
+	long long first = 0;
+	long long second = 1;
+
+	long long res = remainder;
+
+	for (int i = 1; i < remainder; i++) {
+		res = (first + second) % m;
+		first = second;
+		second = res;
+	}
+
+	return res % m;
+}
+
+
+int main() {
+	long long n;
+	cin >> n;
+	cout << (solve(n + 1,10)*solve(n,10)) % 10 << endl;
+}
