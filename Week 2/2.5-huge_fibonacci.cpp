f267487a352805057316209fa2566a08775b9bd8
Pranav Bakale <78615142+pranavbakale@users.noreply.github.com>
2021-09-06 21:18:50 +0530

    Create 2.5-huge_fibonacci.cpp

---
+++ Week 2/2.5-huge_fibonacci.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+long long periodic(long long m) {
+	long long a = 0, b = 1, c = a + b;
+	for (int i = 0; i < m*m; i++) {
+		c = (a + b) % m;
+		a = b;
+		b = c;
+		if (a == 0 && b == 1)
+			return i + 1;
+	}
+}
+int solve(long long n, long long m) {
+	long long remainder = n % periodic(m);
+
+	long long first = 0;
+	long long second = 1;
+
+	long long res = remainder;
+
+	for (int i = 1; i < remainder; i++) {
+		res = (first + second) % m;
+		first = second;
+		second = res;
+	}
+
+	return res % m;
+}
+int main() {
+	long long n, m;
+	cin >> n >> m;
+	cout << solve(n, m) << endl;
+}
